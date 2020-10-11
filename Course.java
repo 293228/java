36cf03db2594593f4d94d08408abcaa7e4bd7f59
293228 <61650327+293228@users.noreply.github.com>
2020-10-11 16:23:55 +0800

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,13 @@
+package Course;
+
+public class Course {
+	
+    public static void main(String args[]) {
+	    int serialNumber=10010;
+	    String curriculumName="数学";
+	    String theTeacher="李华";
+	    System.out.println("serialNumber is "+serialNumber);
+	    System.out.println("curriculumName is "+curriculumName);
+	    System.out.println("theTeacher is "+theTeacher);
+}
+}
